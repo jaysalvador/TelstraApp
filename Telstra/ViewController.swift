ba5d1aa4a9479272c83b180389febe9a5954d805
Jay Andrae Salvador <jay.andrae@playsport.com>
2020-04-02 17:08:49 +1100

    Remove Storyboard

--- Telstra/ViewController.swift
+++ Telstra/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import TelstraAPI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .systemBlue
+        
+        let client = FeedClient()
+        
+        client?.getFeed { (response) in
+            
+            switch response {
+            case .success(let feed):
+                
+                print(feed)
+            case .failure(let error):
+                
+                print(error)
+            }
+        }
     }
 
 
