f1bce8758bef660b1a7f05da2bf76f66c0550906
Jay Salvador <jay.andrae.salvador@gmail.com>
2020-04-07 22:48:52 +1000

    ViewItem enum test

--- TelstraTests/TelstraTests.swift
+++ TelstraTests/TelstraTests.swift
@@ -36,6 +36,36 @@ class TelstraTests: XCTestCase {
         
         XCTAssertNotEqual(contents?[0], contents?[1], "Equality test: Content 0 and 1 must not be equal")
     }
+    
+    func testViewItemEnum() {
+        
+        let feed = getMockFeed()
+        
+        let contents = feed?.rows
+        
+        var items = [ViewItem]()
+        
+        contents?.forEach {
+            
+            items.append(.item($0))
+        }
+        
+        XCTAssertEqual(items[0], items[0], "Equality test failed")
+        
+        XCTAssertNotEqual(items[0], items[1], "Equality test: Item 0 and 1 must not be equal")
+        
+        
+        if case .item(let content) = items[0], let firstContent = contents?.first {
+
+            XCTAssertEqual(content.title, "Beavers", "Title must be beavers")
+            
+            XCTAssertEqual(items[0], ViewItem.item(firstContent), "Equality test failed")
+        }
+        else {
+            
+            XCTAssert(false, "Item was not set properly")
+        }
+    }
 
     private func getMockFeed() -> Feed? {
         
